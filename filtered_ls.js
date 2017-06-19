a6b37fb6fc97ffd7361a9afc7fed414b255a23c7
Roboneet <neethumariyajoy@gmail.com>
2017-06-19 17:06:59 +0530

    filtered_ls working

--- filtered_ls.js
+++ filtered_ls.js
@@ -3,6 +3,9 @@ fs.readdir(process.argv[2], function(err, list){
 	if(err) throw err;
 	var filtered_list = list.filter(function(item, index){
 		extension = item.split('.').slice(-1);
-		return extension[0] == process.argv[3]
+		return extension[0] == process.argv[3] && item.split('.').length > 1; 
+	})
+	filtered_list.forEach(function(ele, index){
+		console.log(ele)
 	})
 })
