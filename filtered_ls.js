ee8d4cd2c259468439dca0d550e86fe84eb96283
Roboneet <neethumariyajoy@gmail.com>
2017-06-19 16:58:28 +0530

    initial commit

---
+++ filtered_ls.js
@@ -0,0 +1,8 @@
+var fs = require("fs");
+fs.readdir(process.argv[2], function(err, list){
+	if(err) throw err;
+	var filtered_list = list.filter(function(item, index){
+		extension = item.split('.').slice(-1);
+		return extension[0] == process.argv[3]
+	})
+})
