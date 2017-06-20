5c80505d25e52e8c130a8ea345ae0439f6d36c0c
Roboneet <neethumariyajoy@gmail.com>
2017-06-20 18:09:05 +0530

    http uppercaser , phew

---
+++ http_uppercaser.js
@@ -0,0 +1,17 @@
+var http = require("http");
+var map = require("through2-map")
+
+
+var server = http.createServer(function(request, response){
+	if(request.method == 'POST'){
+		
+		request.pipe(map(function(chunk){
+			return chunk.toString().toUpperCase();
+		})).pipe(response)
+	
+	}
+	else{
+	response.end()
+	}
+})
+server.listen(process.argv[2])
